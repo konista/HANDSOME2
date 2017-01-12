ea5f4ea6d18a73331cf64fdd8550668d624fbb1d
konis <konis@KONISTA>
2017-01-12 19:33:30 +0800

    添加项目文件。

---
+++ HANDSOME2/Handsome2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HANDSOME2
+{
+    public partial class Handsome2 : Form
+    {
+        public Handsome2()
+        {
+            InitializeComponent();
+        }
+
+        private void Handsome2_Load(object sender, EventArgs e)
+        {
+            LoadMod();
+        }
+
+        private void LoadMod()
+        {
+
+        }
+    }
+}

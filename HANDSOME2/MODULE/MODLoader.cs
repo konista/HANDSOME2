d3ce047b63395b465b05d33aca4ef1488112c1e7
Liuxm <konista@163.com>
2017-01-15 21:35:04 +0800

    0.0.2

--- HANDSOME2/MODULE/MODLoader.cs
+++ HANDSOME2/MODULE/MODLoader.cs
@@ -3,13 +3,179 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace HANDSOME2.MODULE
 {
-    class MODLoader
+    class ModLoader
     {
-        public void Load(string filepath)
+        public struct Var
         {
+            public string name { get; set; }
+            public string type { get; set; }
+            public string max { get; set; }
+            public string min { get; set; }
+        }
+        public struct Subject
+        {
+            public string name { get; set; }
+            public string template { get; set; }
+            public List<Var> vars { get; set; }
+            public List<string> answer { get; set; }
+            public int empty_num { get; set; }
+            public List<List<string>> conditions { get; set; }
+            public string assert { get; set; }
+            public int timeout { get; set; }
+        }
+        public Dictionary<string, Subject> ds = new Dictionary<string, Subject>();
+        public ModLoader(string type)
+        {
+            try
+            {
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.IgnoreComments = true;
+                XmlReader reader = XmlReader.Create(System.AppDomain.CurrentDomain.BaseDirectory + type + ".xml", settings);
+                XmlDocument xd = new XmlDocument();
+                xd.Load(reader);
+                reader.Close();
+                Parse(xd);
+            }
+            catch
+            { }
+            finally
+            { }
+        }
+
+        private void Parse(XmlDocument doc)
+        {
+            XmlNode root = doc.SelectSingleNode("SUBJECTS");
+            XmlNodeList xns = root.ChildNodes;
+            foreach (XmlNode xn in xns)
+            {
+                if (xn.Name != "SUBJECT") { continue; }
+                Subject s = ParseSubject(xn);
+                ds.Add(s.name, s);
+            }
+        }
+
+        private Subject ParseSubject(XmlNode subject)
+        {
+            Subject s = new Subject();
+            s.name = GetSubjectName(subject);
+            XmlNodeList xnl = subject.ChildNodes;
+            foreach (XmlNode xn in xnl)
+            {
+                switch (xn.Name)
+                {
+                    case "TEMPLATE":
+                        {
+                            s.template = GetTemplate(xn);
+                            break;
+                        }
+                    case "VARS":
+                        {
+                            s.vars = GetVars(xn);
+                            break;
+                        }
+                    case "ANSWER":
+                        {
+                            s.answer = GetAnswer(xn);
+                            s.empty_num = GetEmptyNum(xn);
+                            break;
+                        }
+                    case "CONDITION":
+                        {
+                            s.conditions = GetConditions(xn);
+                            break;
+                        }
+                    case "ASSERT":
+                        {
+                            s.assert = GetAssert(xn);
+                            break;
+                        }
+                    case "TIMEOUT":
+                        {
+                            s.timeout = GetTimeout(xn);
+                            break;
+                        }
+                    default:
+                        { break; }
+                }
+            }
+            return s;
+        }
+
+        private string GetSubjectName(XmlNode subject)
+        {
+            return GetAttribute(subject, "name");
+        }
+
+        private string GetTemplate(XmlNode template)
+        {
+            return template.InnerText;
+        }
+
+        private List<Var> GetVars(XmlNode vars)
+        {
+            List<Var> lv = new List<Var>();
+            XmlNodeList xnl = vars.ChildNodes;
+            foreach (XmlNode xn in xnl)
+            {
+                if (xn.Name != "VAR") { continue; }
+                Var var = new Var();
+                var.type = GetAttribute(xn, "type");
+                switch (var.type)
+                {
+                    case "int":
+                        {
+                            var.name = xn.InnerText;
+                            var.max = GetAttribute(xn, "max");
+                            var.min = GetAttribute(xn, "min");
+                            break;
+                        }
+                    default:
+                        { break; }
+                }
+                lv.Add(var);
+            }
+            return lv;
+        }
+
+        private List<string> GetAnswer(XmlNode answer)
+        {
+            return answer.InnerText.Split(new char[1] { '|' }).ToList<string>();
+        }
+
+        private int GetEmptyNum(XmlNode answer)
+        {
+            return int.Parse(GetAttribute(answer, "empty_num"));
+        }
+
+        private List<List<string>> GetConditions(XmlNode conditions)
+        {
+            List<List<string>> ls = new List<List<string>>();
+            XmlNodeList xnl = conditions.ChildNodes;
+            foreach (XmlNode xn in xnl)
+            {
+                if (xn.Name != "CONDITION") { continue; }
+                ls.Add(xn.InnerText.Split(new char[1] { ',' }).ToList<string>());
+            }
+            return ls;
+        }
+
+        private string GetAssert(XmlNode assert)
+        {
+            return assert.InnerText;
+        }
+
+        private int GetTimeout(XmlNode timeout)
+        {
+            return int.Parse(timeout.InnerText);
+        }
+
+        private string GetAttribute(XmlNode xn, string attr)
+        {
+            return ((XmlElement)xn).GetAttribute(attr);
         }
     }
 }
---
+++ HANDSOME2/MODULE/SubjectCreator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HANDSOME2.MODULE
+{
+    class SubjectCreator
+    {
+        public Dictionary<string, ModLoader.Subject> ds;
+        public SubjectCreator(string type)
+        {
+            ModLoader ml = new ModLoader(type);
+            ds = ml.ds;
+        }
+        public void CreateSubject(string sbj_name)
+        {
+            ModLoader.Subject s = ds[sbj_name];
+            string ret = s.template;
+            Dictionary<string, object> vars = MakeVars(s);
+            if (Assert(vars, s.conditions))
+            {
+
+            }
+            else
+            { CreateSubject(sbj_name); }
+        }
+        private Dictionary<string,object> MakeVars(ModLoader.Subject s)
+        {
+            Dictionary<string, object> vars = new Dictionary<string, object>();
+            foreach (ModLoader.Var var in s.vars)
+            {
+                switch (var.type)
+                {
+                    case "int":
+                        {
+                            vars.Add(var.name, RandomInt(var.min, var.max));
+                            break;
+                        }
+                    default:
+                        { break; }
+                }
+            }
+            return vars;
+        }
+        private bool Assert(Dictionary<string,object> vars, List<List<string>> conditions)
+        {
+            bool ret = false;
+            return ret;
+        }
+        private int RandomInt(string min, string max)
+        {
+            int imin = int.Parse(min);
+            int imax = int.Parse(max);
+            Random r = new Random();
+            return r.Next(imin, imax);
+        }
+    }
+}
